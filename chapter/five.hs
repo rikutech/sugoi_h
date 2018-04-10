b43560d953445372eb4628496d707100ebe946cf
rikutech <rikutech@gmail.com>
2018-04-11 01:13:58 +0900

    try chapter 5-3

--- chapter/five.hs
+++ chapter/five.hs
@@ -7,3 +7,18 @@ zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
 zipWith' _ [] _ = []
 zipWith' _ _ [] = []
 zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
+
+-- 5-3
+
+-- mapの実装
+
+map' :: (a -> b) -> [a] -> [b]
+map' _ [] = []
+map' f (x:xs) = f x : map' f xs
+
+-- filterの実装
+filter' :: (a -> Bool) -> [a] -> [a]
+filter' _ [] = []
+filter' f (x:xs)
+  | f x = x : filter' f xs
+  | otherwise = filter' f xs
