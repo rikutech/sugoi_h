2c4aaf10e5d0ed7dd825b6ee3f48971fa1ba85a2
rikutech <rikutech@gmail.com>
2018-04-11 00:15:22 +0900

    try recursion

--- scripts/test.hs
+++ scripts/test.hs
@@ -3,7 +3,7 @@ head' :: [a] -> a
 head' [] = error "err"
 head' (x:_) = x
 
--- ガードとwhere
+-- ガードとwhere(whereで束縛した変数)
 bmiTell :: Double -> Double -> String
 bmiTell weight height
   | bmi <= skinny = "痩せすぎです"
@@ -14,3 +14,18 @@ bmiTell weight height
         skinny = 18.5
         normal = 25.0
         fat = 30.0
+
+-- takeの実装
+
+take' :: Int -> [a] -> [a]
+take' n _
+  | n <= 0 = []
+take' _ [] = []
+take' n (x:xs) = x : take' (n - 1) xs
+
+quicksort :: (Ord a) => [a] -> [a]
+quicksort [] = []
+quicksort (x:xs) =
+  let lessOrEqual = [a | a <- xs, a <= x]
+      grater = [a | a <- xs, a > x]
+  in quicksort lessOrEqual ++ [x] ++ quicksort grater
